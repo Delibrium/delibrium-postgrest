fbbc9ec8f3b2d49e4980eed858e3b181749b7ffc
Vincent Ahrend <mail@vincentahrend.com>
2018-11-14 12:43:59 +0100

    Remove temp file

--- create_default_categories.sql
+++
@@ -1,8 +0,0 @@
--- This is assignung them to school id 1
-
-insert into category (school_id, name, description) values (1, 'Regeln', '');
-insert into category (school_id, name, description) values (1, 'Ausstattung', '');
-insert into category (school_id, name, description) values (1, 'Aktivitäten', '');
-insert into category (school_id, name, description) values (1, 'Unterricht', '');
-insert into category (school_id, name, description) values (1, 'Zeit', '');
-insert into category (school_id, name, description) values (1, 'Umgebung', '');

a3e9cea244089dc78901f8c8002ad61f5b85c7ae
Edgar Z. Alvarenga <e@vaz.io>
2018-11-14 14:44:42 +0100

    Fix create_school RPC function

--- rpc.sql
+++ rpc.sql
@@ -3,32 +3,32 @@ create or replace language plpython3u;
 
 
 create or replace function aula.create_school(name text, config jsonb default null)
-    returns json
+    returns void
     language plpython3u
 as $$
     result = plpy.execute("""
-        insert into aula.school ( name, config ) values ( '{name}', '{config}' );
+        insert into aula.school ( name, config ) values ( '{name}', '{config}' ) returning id;
     """.format(
         name=name,
         config=config
     ))
 
-    plpy.info(result)
+    plpy.info('RESULT', result[0])
 
-    school_id = result[0].id
+    school_id = result[0]['id']
 
     result2 = plpy.execute("""
-        insert into category (school_id, name, description) values ({school_id}, 'Regeln', '');
-        insert into category (school_id, name, description) values ({school_id}, 'Ausstattung', '');
-        insert into category (school_id, name, description) values ({school_id}, 'Aktivitäten', '');
-        insert into category (school_id, name, description) values ({school_id}, 'Unterricht', '');
-        insert into category (school_id, name, description) values ({school_id}, 'Zeit', '');
-        insert into category (school_id, name, description) values ({school_id}, 'Umgebung', '');
-    """.format(school_id))
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Regeln', '');
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Ausstattung', '');
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Aktivitäten', '');
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Unterricht', '');
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Zeit', '');
+        insert into aula.category (school_id, name, description) values ({school_id}, 'Umgebung', '');
+    """.format(school_id=school_id))
 
-    return result
+    return
+$$;
 
-    
 create or replace function aula.quorum_info(school_id bigint, space_id bigint default null)
   returns json
   language plpython3u
@@ -36,8 +36,8 @@ as $$
     import json
 
     result = plpy.execute("""
-        select config 
-        from aula.school 
+        select config
+        from aula.school
         where id = {};
     """.format(school_id))
 
@@ -46,7 +46,7 @@ as $$
         return
 
     config = json.loads(result[0]['config'])
-    
+
     if 'classQuorum' not in config:
         config = {
             'schoolQuorum': 30,
@@ -55,16 +55,16 @@ as $$
 
     if space_id is None:
         usercount = plpy.execute("""
-            select count(distinct user_id) 
-            from aula.user_group 
+            select count(distinct user_id)
+            from aula.user_group
             where school_id={};
         """.format(school_id))[0]['count']
         config['totalVoters'] = usercount
         quorum_threshold = 0.01 * int(config['schoolQuorum'])
     else:
         usercount = plpy.execute("""
-            select count(distinct user_id) 
-            from aula.user_group 
+            select count(distinct user_id)
+            from aula.user_group
             where group_id='student'
             and idea_space={};
         """.format(space_id))[0]['count']

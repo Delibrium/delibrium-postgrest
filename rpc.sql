de841f8485cab3d296d2d4a5a72dd43f61129d89
Vincent Ahrend <mail@vincentahrend.com>
2018-11-28 13:55:07 +0100

    Add RPC for updating user

--- rpc.sql
+++ rpc.sql
@@ -19,7 +19,6 @@ res_school_id = plpy.execute(
 )
 if len(res_school_id) == 0:
     plpy.error('Current user is not associated with a school.', sqlstate='PT401')
-
 school_id = res_school_id[0]['current_setting']
 
 res_calling_user_id = plpy.execute(
@@ -27,7 +26,6 @@ res_calling_user_id = plpy.execute(
 )
 if len(res_calling_user_id) == 0:
     plpy.error('Did not find user associated with this request.', sqlstate='PT401')
-
 calling_user_id = res_calling_user_id[0]['current_setting']
 
 def create_random_password():
@@ -98,6 +96,53 @@ q4 = "update aula_secure.user_login set aula_user_id={} where id={};".format(
 plpy.execute(q4)
 $$;
 
+create or replace function aula.update_user(
+    id bigint,
+    first_name text, 
+    last_name text, 
+    username text,
+    email text default null
+) returns void language plpython3u
+as $$
+    res_school_id = plpy.execute(
+        "select current_setting('request.jwt.claim.school_id');"
+    )
+    if len(res_school_id) == 0:
+        plpy.error('Current user is not associated with a school.', sqlstate='PT401')
+    school_id = res_school_id[0]['current_setting']
+
+    res_calling_user_id = plpy.execute(
+        "select current_setting('request.jwt.claim.user_id');"
+    )
+    if len(res_calling_user_id) == 0:
+        plpy.error('Did not find user associated with this request.', sqlstate='PT401')
+    calling_user_id = res_calling_user_id[0]['current_setting']
+
+    q = """update 
+    aula.users set 
+        first_name='{}',
+        last_name='{}',
+        email='{}',
+        changed_by='{}',
+        changed_at=now() 
+    where
+        id='{}' 
+    returning user_login_id;""".format(
+        first_name,
+        last_name,
+        email or '',
+        calling_user_id,
+        id
+    )
+    res = plpy.execute(q)
+    login_id = res[0]['user_login_id']
+
+    q2 = "update aula_secure.user_login set login='{}' where id='{}';".format(
+        username, login_id
+    )
+    plpy.execute(q2)
+$$;
+
 create or replace function aula.user_listing()
     returns json
     language plpython3u
@@ -115,11 +160,11 @@ as $$
             us.*,
             ul.config,
             ul.login,
-            ug.group_id,
-            ug.idea_space,
+                ug.group_id, 
+                ug.idea_space,
             (select sp.title as idea_space_title 
-                from aula.idea_space as sp 
-                where sp.id=ug.idea_space)
+                    from aula.idea_space as sp 
+                    where sp.id=ug.idea_space)
         from
             aula.users as us,
             aula.user_group as ug,

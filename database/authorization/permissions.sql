bd5e9e5ea0dea0ad49925159987c7a67931c7fbb
Edgar Z. Alvarenga <e@vaz.io>
2019-01-21 12:58:57 +0100

    Correct permissions for tables

--- database/authorization/permissions.sql
+++ database/authorization/permissions.sql
@@ -1,19 +1,92 @@
 -- by default all functions are accessible to the public, we need to remove that and define our specific access rules
 revoke all privileges on function aula.refresh_token() from public;
 
-create policy school_admin_users on aula.users using (aula.is_admin(school_id) or aula.is_owner(id)) with check (aula.is_admin(school_id) or aula.is_owner(id));
-create policy school_admin_school on aula.school using (aula.is_admin(id)) with check (aula.is_admin(id));
-create policy school_admin_idea_space on aula.idea_space using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_topic on aula.topic using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_feasible on aula.feasible using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_idea on aula.idea using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_idea_like on aula.idea_like using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_idea_vote on aula.idea_vote using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_comment on aula.comment using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_comment_vote on aula.comment_vote using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
-create policy school_admin_school_class on aula.school_class using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
+drop policy if exists school_admin_school_class on aula.school_class;
+create policy school_admin_school_class on aula.school_class using (aula.is_admin(school_id) or aula.from_school(school_id)) with check (aula.is_admin(school_id));
+
+drop policy if exists school_admin_user_group on aula.user_group;
 create policy school_admin_user_group on aula.user_group using (aula.is_admin(school_id) or aula.is_owner(user_id)) with check (aula.is_admin(school_id) or aula.is_owner(user_id));
-create policy school_admin_delegation on aula.delegation using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
+
+drop policy if exists school_admin_school on aula.school;
+create policy school_admin_school on aula.school using (aula.is_admin(id)) with check (aula.is_admin(id));
+
+drop policy if exists school_admin_idea_space on aula.idea_space;
+create policy school_admin_idea_space on aula.idea_space using (aula.is_admin(school_id) or aula.from_school(school_id)) with check (aula.is_admin(school_id));
+
+drop policy if exists school_admin_topic on aula.topic;
+create policy school_admin_topic on aula.topic using (aula.is_admin(school_id) or aula.from_school(school_id)) with check (aula.is_admin(school_id));
+
+drop policy if exists school_admin_feasible on aula.feasible;
+create policy school_admin_feasible on aula.feasible using (aula.is_admin(school_id) or aula.from_school(school_id)) with check (aula.is_admin(school_id));
+
+-- Idea
+drop policy if exists school_select_idea on aula.idea;
+create policy school_select_idea on aula.idea for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_idea on aula.idea;
+create policy school_create_idea on aula.idea for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_idea on aula.idea;
+create policy school_update_idea on aula.idea for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_idea on aula.idea;
+create policy school_delete_idea on aula.idea for delete using (aula.is_admin(school_id));
+
+-- Users TODO: Create view to restrict columns
+
+drop policy if exists school_admin_users on aula.users;
+create policy school_admin_users on aula.users using (aula.is_admin(school_id) or aula.from_school(school_id) or aula.is_owner(id)) with check (aula.is_admin(school_id) or aula.is_owner(id));
+
+-- Idea Like
+drop policy if exists school_select_idea_like on aula.idea_like;
+create policy school_select_idea_like on aula.idea_like for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_idea_like on aula.idea_like;
+drop policy if exists school_create_idea_like on aula.idea_like;
+create policy school_create_idea_like on aula.idea_like for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_idea_like on aula.idea_like;
+create policy school_update_idea_like on aula.idea_like for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_idea_like on aula.idea_like;
+create policy school_delete_idea_like on aula.idea_like for delete using (aula.is_admin(school_id));
+
+-- Idea Vote
+drop policy if exists school_select_idea_vote on aula.idea_vote;
+create policy school_select_idea_vote on aula.idea_vote for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_idea_vote on aula.idea_vote;
+create policy school_create_idea_vote on aula.idea_vote for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_idea_vote on aula.idea_vote;
+create policy school_update_idea_vote on aula.idea_vote for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_idea_vote on aula.idea_vote;
+create policy school_delete_idea_vote on aula.idea_vote for delete using (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+
+
+-- Comment
+drop policy if exists school_select_comment on aula.comment;
+create policy school_select_comment on aula.comment for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_comment on aula.comment;
+create policy school_create_comment on aula.comment for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_comment on aula.comment;
+create policy school_update_comment on aula.comment for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_comment on aula.comment;
+create policy school_delete_comment on aula.comment for delete using (aula.is_admin(school_id));
+
+-- Comment Vote
+drop policy if exists school_select_comment_vote on aula.comment_vote;
+create policy school_select_comment_vote on aula.comment_vote for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_comment_vote on aula.comment_vote;
+create policy school_create_comment_vote on aula.comment_vote for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_comment_vote on aula.comment_vote;
+create policy school_update_comment_vote on aula.comment_vote for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_comment_vote on aula.comment_vote;
+create policy school_delete_comment_vote on aula.comment_vote for delete using (aula.is_admin(school_id));
+
+
+-- Delegation
+drop policy if exists school_select_delegation on aula.delegation;
+create policy school_select_delegation on aula.delegation for select using (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_create_delegation on aula.delegation;
+create policy school_create_delegation on aula.delegation for insert with check (aula.is_admin(school_id) or (aula.from_school(school_id)));
+drop policy if exists school_update_delegation on aula.delegation;
+create policy school_update_delegation on aula.delegation for update with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
+drop policy if exists school_delete_delegation on aula.delegation;
+create policy school_delete_delegation on aula.delegation for delete using (aula.is_admin(school_id));
+
 drop policy if exists user_login on aula_secure.user_login ;
 create policy user_login on aula_secure.user_login using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));
 -- create policy admin_user_listing on aula.user_listing using (aula.is_admin(school_id)) with check (aula.is_admin(school_id));

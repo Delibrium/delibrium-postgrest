7b480bb47bed9850d18e121cd1f132514a4a7c4d
Edgar Z. Alvarenga <e@vaz.io>
2019-01-23 00:45:05 +0100

    Corect policy

--- database/authorization/permissions.sql
+++ database/authorization/permissions.sql
@@ -74,7 +74,7 @@ create policy school_create_comment_vote on aula.comment_vote for insert with ch
 drop policy if exists school_update_comment_vote on aula.comment_vote;
 create policy school_update_comment_vote on aula.comment_vote for update using (aula.is_admin(school_id) or (aula.from_school(school_id))) with check (aula.is_admin(school_id) or (aula.is_owner(created_by)));
 drop policy if exists school_delete_comment_vote on aula.comment_vote;
-create policy school_delete_comment_vote on aula.comment_vote for delete using (aula.is_admin(school_id) or (aula.from_school(school_id))) using (aula.is_admin(school_id));
+create policy school_delete_comment_vote on aula.comment_vote for delete using (aula.is_admin(school_id) or (aula.is_owner(school_id))) with check (aula.is_admin(school_id));
 
 
 -- Delegation

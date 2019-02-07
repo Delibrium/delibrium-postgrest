6297f974e070fdc3cc6669bece4b13516e3eadf5
Edgar Z. Alvarenga <e@vaz.io>
2019-02-08 00:05:25 +0100

    Add idea space id on 'me' profile endpoint

--- database/authorization/me.sql
+++ database/authorization/me.sql
@@ -12,5 +12,18 @@ if maybe_user_id:
   usr['role_details'] = list(roles)
   usr['role'] = list(set([r['group_id'] for r in roles]))
 
+  plpy.execute('set "request.jwt.claim.user_group" TO \'admin\'')
+  rv = plpy.execute('select group_id,idea_space from aula.user_group where user_id = {}'.format(maybe_user_id))
+
+  all_roles = []
+  for r in rv:
+    role = [r['group_id']]
+    space = r['idea_space']
+    if space:
+      role += [space]
+    all_roles += [role]
+
+  usr['roles'] = all_roles
+
   return json.dumps(usr)
 $$;

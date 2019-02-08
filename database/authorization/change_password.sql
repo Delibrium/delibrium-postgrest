88d0b964d589b656385f75649ae86a1e1b0cdde7
Edgar Z. Alvarenga <e@vaz.io>
2019-02-08 15:25:10 +0100

    Return error on endpoint to change user password

--- database/authorization/change_password.sql
+++ database/authorization/change_password.sql
@@ -2,8 +2,22 @@ create or replace function aula_secure.change_password(user_id bigint, old_passw
    returns void
    language plpgsql
 as $$
- begin
+declare
+  changes_num integer;
+begin
  raise info 'CHANGE PASS: %, %, user_group => %, user_id => %, school_id => %', old_password, new_password, current_setting('request.jwt.claim.user_group', true),current_setting('request.jwt.claim.user_id', true), current_setting('request.jwt.claim.school_id', true);
- update aula_secure.user_login set password = new_password where id = user_id and password = crypt(old_password, aula_secure.user_login.password);
- end
+ -- update aula_secure.user_login set password = new_password where id = user_id and password = crypt(old_password, aula_secure.user_login.password);
+ with row_changes as (
+  update aula_secure.user_login set password = new_password
+    where
+      id = user_id
+      and
+      password = crypt(old_password, password) returning 1)
+ select count(*) from row_changes into changes_num;
+
+ if changes_num = 0 then
+  raise exception 'Check password';
+ end if;
+
+end
 $$;
--- database/model/idea_permissions.sql
+++ database/model/idea_permissions.sql
@@ -4,10 +4,13 @@ create policy school_select_idea on aula.idea for select using (aula.is_admin(sc
 
 drop policy if exists school_create_idea on aula.idea;
 create policy school_create_idea on aula.idea for insert with check (
+  -- If user is admin, or admin for the school
   aula.is_admin(school_id) or
   (
+  -- of if user is from the school and wants to create an idea on school level
     ((aula.from_school(school_id) and idea_space is null) and
       (created_by = request.user_id()))
+  -- of if user wants to create an idea on ideas space the he belongs as student
     or
     ((aula.from_space('student', idea_space)) and
       (created_by = request.user_id()))

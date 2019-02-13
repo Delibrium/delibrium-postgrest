6c3a83f32d30592ab5cebc4750f2a57b74ff8229
Edgar Z. Alvarenga <e@vaz.io>
2019-02-13 13:14:59 +0100

    Reset hashed password also

--- database/authorization/reset_password.sql
+++ database/authorization/reset_password.sql
@@ -8,7 +8,7 @@ begin
   if (aula.is_admin(school_id)) then
     begin
       select aula.random_password() into new_password;
-      update aula_secure.user_login set config = jsonb_set(config, '{temp_password}', to_jsonb(new_password)) where aula_user_id = user_id;
+      update aula_secure.user_login set config = jsonb_set(config, '{temp_password}', to_jsonb(new_password)), password = new_password where aula_user_id = user_id;
     end;
 
     return json_build_object('new_password', new_password);

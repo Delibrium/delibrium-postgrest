c72fba1f15347155af403cc2ccf8086a241465ca
Edgar Z. Alvarenga <e@vaz.io>
2019-02-13 11:40:30 +0100

    Remove temporary password when user changes it

--- database/authorization/change_password.sql
+++ database/authorization/change_password.sql
@@ -5,19 +5,18 @@ as $$
 declare
   changes_num integer;
 begin
- raise info 'CHANGE PASS: %, %, user_group => %, user_id => %, school_id => %', old_password, new_password, current_setting('request.jwt.claim.user_group', true),current_setting('request.jwt.claim.user_id', true), current_setting('request.jwt.claim.school_id', true);
- -- update aula_secure.user_login set password = new_password where id = user_id and password = crypt(old_password, aula_secure.user_login.password);
- with row_changes as (
-  update aula_secure.user_login set password = new_password
-    where
-      id = user_id
-      and
-      password = crypt(old_password, password) returning 1)
- select count(*) from row_changes into changes_num;
+  with row_changes as (
+    update aula_secure.user_login
+      set password = new_password, config = config #- '{temp_password}'
+      where
+        id = user_id
+        and
+        password = crypt(old_password, password) returning 1)
+  select count(*) from row_changes into changes_num;
 
- if changes_num = 0 then
-  raise exception 'Check password';
- end if;
+  if changes_num = 0 then
+   raise exception 'Check password';
+  end if;
 
 end
 $$;

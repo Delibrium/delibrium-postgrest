04a9be9f1e07bd9a727552ebe7a1894b51ca14d5
Edgar Z. Alvarenga <e@vaz.io>
2019-02-14 21:11:57 +0100

    Import default categories on database creation

--- database/data/init.sql
+++ database/data/init.sql
@@ -1,5 +1,9 @@
+\set base_dir `echo $DIR`
+\set categories_path '\'' :base_dir '/database/data/categories.csv\''
+\set school_path '\'' :base_dir '/database/data/school.csv\''
+
 -- Insert special school
-insert into aula.school(id, name) values (1, 'aula');
+copy aula.school (id,created_at, changed_at, name, config, created_by) from :school_path with (delimiter ',', format csv, quote '"', header true);
 
 -- Insert admin user
 insert into aula_secure.user_login (school_id, login, password) values ( 1, 'admin', 'password');
@@ -12,5 +16,7 @@ insert into aula.users (school_id, user_login_id, first_name, last_name, changed
 insert into aula.user_group (school_id, user_id, group_id) values(1, 2, 'student');
 
 -- Insert Class
-insert into aula.idea_space (school_id, created_by, changed_by, title, description, slug) values (1,1,1,'Class', 'Test class', 'test');
+insert into aula.idea_space (school_id, created_by, changed_by, title, description, slug) values (1,1,1,'Klass', 'Test Klass', 'Test Klass');
 
+-- Import categories
+copy aula.category (id,school_id, name, description, image,def,position) from :categories_path with (delimiter ',', format csv, quote '"', header true);
